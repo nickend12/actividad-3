672f0b550888264a6fddc8b659dbf5d0f41ae5d5
oscar <oscar.garciajg@est.iudigital.edu.co>
2023-12-03 17:58:50 -0500

    index

--- script.js
+++ script.js
@@ -6,18 +6,18 @@ document.addEventListener("DOMContentLoaded", function() {
     var enviadoConExito = false;
 
     formulario.addEventListener("submit", function(event) {
-        // Evitar que el formulario se envíe por defecto
+       
         event.preventDefault();
 
-        // Limpiar mensajes de error y éxito anteriores
+       
         erroresDiv.innerHTML = "";
 
-        // Realizar validaciones
+    
         var nombre = document.getElementById("nombre").value;
         var price = document.getElementById("price").value;
         var inventary = document.getElementById("inventary").value;
 
-        // Validar campos obligatorios
+        
         if (!nombre) {
             mostrarError("El campo Nombre es obligatorio");
         }
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function() {
             mostrarError("El campo Inventario debe ser un número válido y mayor o igual a cero");
         }
 
-        // Si no hay errores, mostrar mensaje de éxito y marcar como enviado con éxito
+        
         if (erroresDiv.innerHTML === "") {
             mostrarExito();
         }
@@ -50,18 +50,18 @@ document.addEventListener("DOMContentLoaded", function() {
         alert("¡Producto creado con éxito!");
         exitoDiv.style.display = "block";
 
-        // Agregar un tiempo de espera antes de realizar acciones adicionales
+       
         setTimeout(function() {
             exitoDiv.style.display = "none";
-            formulario.reset();  // Opcional: reiniciar el formulario después del éxito
-        }, 3000); // Ocultar después de 3 segundos (ajusta según tu preferencia)
+            formulario.reset();  
+        }, 3000);
     }
 
-    // Evitar que el formulario se envíe nuevamente al presionar Enter
+    
     formulario.addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
             event.preventDefault();
-            // Si el formulario fue enviado con éxito, no resetearlo al presionar Enter
+            
             if (!enviadoConExito) {
                 formulario.reset();
             }
