b0f36c2207cdfebded4ee60f271d988db49219dc
oscar <oscar.garciajg@est.iudigital.edu.co>
2023-12-09 14:08:43 -0500

    index

--- card.js
+++ card.js
@@ -1,28 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const carLink = document.querySelector('a[href="car.html"]');
-    carLink.addEventListener('click', mostrarCarrito);
-  });
-  
-  function mostrarCarrito(event) {
-    event.preventDefault();
-    const carrito = arregloProductos.map(producto => ({
-      ...producto,
-      total: producto.precio * producto.cantidad
-    }));
-  
-    const carritoHTML = carrito.map(producto => `
-      <div>
-        <img src="${producto.imagen}" alt="${producto.nombre}">
-        <p>${producto.nombre}</p>
-        <p>Precio: $${producto.precio}</p>
-        <p>Cantidad: ${producto.cantidad}</p>
-        <p>Total: $${producto.total}</p>
-      </div>
-    `).join('');
-  
-    document.querySelector('main').innerHTML = carritoHTML;
-  }
-  const arregloProductos = [
+const arregloProductos = [
     {
       nombre: 'Producto 1',
       precio: 20,
@@ -37,4 +13,4 @@ document.addEventListener('DOMContentLoaded', function () {
     },
     // Agrega más productos según tus necesidades
   ];
-    
+  
